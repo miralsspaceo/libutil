b483f52f4f6e180dd2ffb125e6973cd5f0bbb969
sotsys358 <mirals.spaceo@gmail.com>
2022-08-31 09:43:51 +0530

    first

---
+++ mylibrary/src/main/java/com/example/mylibrary/MiralActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mylibrary
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.mylibrary.databinding.ActivityMainBinding
+
+class MiralActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+
+
+    }
+
+
+}
